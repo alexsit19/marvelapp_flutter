e216b9c28c848e744e6be1e7667a758498470747
alexsit19 <alexsit12@gmail.com>
2022-04-17 22:09:52 +0300

    add _getThumbnailUrl, _getDescriptionBlock, _getSeriesBlock in details_content

--- lib/presentation/widgets/details_content.dart
+++ lib/presentation/widgets/details_content.dart
@@ -14,38 +14,70 @@ class DetailsContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String description = "";
-    if (character?.description != null) {
-      description = character?.description as String;
-    }
-    return Padding(
-      padding: const EdgeInsets.all(6.0),
-      child: Column(
-        verticalDirection: VerticalDirection.down,
-        children: <Widget>[
-          FadeInImage.assetNetwork(
-            placeholder: "assets/graphics/placeholder-150x150.png",
-            image:
-                "${character?.thumbnailPath}/standard_xlarge.${character?.thumbnailExtension}",
-            height: 300,
-            width: 300,
-            fit: BoxFit.fitHeight,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  character?.name ?? "missing name",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
+    if (character == null) {
+      return const Text(
+        "Information not available",
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Column(
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            FadeInImage.assetNetwork(
+              placeholder: "assets/graphics/placeholder-150x150.png",
+              image: _getThumbnailUrl(),
+              height: 300,
+              width: 300,
+              fit: BoxFit.fitHeight,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    character?.name ?? "missing name",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            _getDescriptionBlock(),
+            _getSeriesBlock(),
+          ],
+        ),
+      );
+    }
+  }
+
+  String _getThumbnailUrl() {
+    String? path = character?.thumbnailPath;
+    String? extension = character?.thumbnailExtension;
+    String format = "standard_xlarge";
+    return "$path/$format.$extension";
+  }
+
+  Widget _getDescriptionBlock() {
+    bool visible = false;
+    print(character?.description);
+    if (character?.description != null && character?.description != "") {
+      visible = true;
+    } else {
+      visible = false;
+    }
+    print(visible);
+    return Visibility(
+      visible: visible,
+      child: Column(
+        children: [
           Padding(
             padding: const EdgeInsets.only(top: 8.0, left: 8.0, bottom: 4.0),
             child: Row(
@@ -63,10 +95,28 @@ class DetailsContent extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
             child: Text(
-              description != "" ? description : "description missing",
+              character?.description as String,
               textAlign: TextAlign.justify,
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getSeriesBlock() {
+    bool visible = false;
+    if (series != null) {
+      if (series!.isNotEmpty) {
+        visible = true;
+      }
+    } else {
+      visible = false;
+    }
+    return Visibility(
+      visible: visible,
+      child: Column(
+        children: [
           Padding(
             padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
             child: Row(
