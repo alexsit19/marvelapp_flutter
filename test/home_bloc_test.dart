2d0efd69a8a373ba238782e23bed7bc11077f212
alexsit19 <alexsit12@gmail.com>
2022-09-11 20:58:10 +0300

    add home_bloc_test.dart

---
+++ test/home_bloc_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:marvelapp_flutter/presentation/features/home/bloc/home_bloc.dart';
+
+void main() {
+
+  group('HomeBlocTest', () {
+    late HomeBloc homeBloc;
+
+  });
+}
